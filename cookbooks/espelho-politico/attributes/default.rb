76e5bb6bcedca3756ee31788aea215a4ede992e2
Ana Carolina Lopes <ana_karolopes@hotmail.com>
2015-06-17 16:09:17 -0300

    Adicionando senha do mysql aos atributos e removendo senhas de debian e mac-os
    Signed-off-by: Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
    Signed-off-by: Ana Carolina Lopes <ana_karolopes@hotmail.com>

--- cookbooks/espelho-politico/attributes/default.rb
+++ cookbooks/espelho-politico/attributes/default.rb
@@ -1,2 +1,4 @@
 default["espelho-politico"]["database"] = "espelho-politico"
 default["espelho-politico"]["db_username"] = "espelho-politico"
+default['espelho-politico']['db_password'] = "root123"
+default['mysql']['server_root_password'] = "root123"
